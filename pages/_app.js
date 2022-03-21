2c9c0bb04540872d9e8dae44cf480de4b58fc408
Deveshb15 <deveshb583@gmail.com>
2022-03-21 12:50:26 +0530

    feat: added tailwind and wagmi

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
 import '../styles/globals.css'
+import { Provider, defaultChains, InjectedConnector } from 'wagmi'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  const connectors = () => {
+    return [
+      new InjectedConnector({ 
+        chains: defaultChains,
+        options: { shimDisconnect: true },
+      }),
+    ];
+};
+
+  return (
+    <Provider autoConnect connectors={connectors}>
+      <Component {...pageProps} />
+    </Provider>
+  ) 
 }
 
 export default MyApp
